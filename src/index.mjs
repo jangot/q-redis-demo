ac39e367d2852920d2e9228f75b857d0f6855697
jangot <pulin.pavel@gmail.com>
2023-05-16 21:07:58 +0200

    add params

--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,27 @@
 import { createClient } from 'redis';
 import { EVENT, Listener } from './Listener.mjs';
 
+const [
+    node,
+    script,
+    producersCount,
+    range
+] = process.argv;
+console.log(producersCount, range);
+if (isNaN(producersCount)) {
+    throw new Error('Producers argument must be a number');
+}
+if (isNaN(range)) {
+    throw new Error('Range argument must be a number');
+}
+
 const client = createClient();
-
 client.on('error', err => console.log('Redis Client Error', err));
-
 await client.connect();
 console.log('Connected')
 
 const QUEUE_NAME = 'my:event';
-const listener = new Listener(client, QUEUE_NAME, 5);
+const listener = new Listener(client, QUEUE_NAME, range);
 
 listener.emitter.on(EVENT.ADD, async (data) => {
     console.log(data);
